89a46f0ec61c10bdb7dd7048173598e02a4207b2
ivonildo lopes rodrigues <ivonildolopes.ti@gmail.com>
2020-03-25 13:50:09 -0300

    acessando servico sem esta inscrito no eureka

---
+++ src/main/java/com/alura/microservice/loja/DTO/AreaDTO.java
@@ -0,0 +1,11 @@
+package com.alura.microservice.loja.DTO;
+
+import lombok.Data;
+
+@Data
+public class AreaDTO {
+	
+	private Long id;
+	private String nome;
+
+}
--- src/main/java/com/alura/microservice/loja/service/serviceImpl/CompraServiceImpl.java
+++ src/main/java/com/alura/microservice/loja/service/serviceImpl/CompraServiceImpl.java
@@ -1,14 +1,23 @@
 package com.alura.microservice.loja.service.serviceImpl;
 
-import com.alura.microservice.loja.DTO.CompraDTO;
-import com.alura.microservice.loja.DTO.InfoFornecedorDTO;
-import com.alura.microservice.loja.service.CompraService;
+import java.io.IOException;
+import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+
+import com.alura.microservice.loja.DTO.AreaDTO;
+import com.alura.microservice.loja.DTO.CompraDTO;
+import com.alura.microservice.loja.DTO.InfoFornecedorDTO;
+import com.alura.microservice.loja.service.CompraService;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class CompraServiceImpl implements CompraService {
@@ -21,28 +30,25 @@ public class CompraServiceImpl implements CompraService {
 
         /**
          * esse trecho de codigo esta acessando outro microservico
+         * inscrito no eureka
          */
         ResponseEntity<InfoFornecedorDTO> exchange =
           client.exchange("http://fornecedor/info/"+ compra.getEndereco().getEstado(),
           HttpMethod.GET,null,InfoFornecedorDTO.class);
         System.out.print(exchange.getBody().getEndereco());
+        
+                
+        /**
+         * Acessando servico sem esta inscrico no eureka
+         */
+        RestTemplate client1 = new RestTemplate();
+        ResponseEntity<AreaDTO> exchange1 =
+        		client1.exchange("http://brq-api.herokuapp.com/api/v1/areas/1",
+                HttpMethod.GET,null,AreaDTO.class);
+        AreaDTO dto = new AreaDTO();
+        System.out.print("==================RETORNO: "+  exchange1.getBody().getNome());
 
     }
     
-    
-    
-//    @Override
-//    public void realizaCompra(CompraDTO compra) {
-//
-//        RestTemplate client = new RestTemplate();
-//        /**
-//         * esse trecho de codigo esta acessando outro microservico
-//         */
-//        ResponseEntity<InfoFornecedorDTO> exchange =
-//        client.exchange("http://localhost:8081/info/"+ compra.getEndereco().getEstado(),
-//                HttpMethod.GET,null,InfoFornecedorDTO.class);
-//
-//        System.out.print(exchange.getBody().getEndereco());
-//
-//    }
+ 
 }
